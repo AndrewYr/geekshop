0addb280bcd3366c4bbac07d43e65d3e9f9110af
AndrewYr <andrewyr93@gmail.com>
2019-03-10 15:53:18 +0300

    ДЗ 6

--- mainApp/views.py
+++ mainApp/views.py
@@ -11,6 +11,10 @@ def index(request):
     return render(request, 'mainApp/index.html', locals())
 
 def products(request, pk=None):
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
     links_menu = ProductCategory.objects.all()
     category = ProductCategory.objects.all()
     products = Product.objects.all()
@@ -25,15 +29,34 @@ def products(request, pk=None):
 
     return render(request, 'mainApp/products.html', locals())
 
-def fishnet_chair(request):
+
+def product(request, pk=None):
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
+    product = Product.objects.filter(id=int(pk))[0]
+    products = Product.objects.filter(category_id=product.category.id)[0:3]
     return render(request, 'mainApp/fishnet_chair.html', locals())
 
 def history(request):
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
     return render(request, 'mainApp/history.html', locals())
 
 def showroom(request):
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
     return render(request, 'mainApp/showroom.html', locals())
 
 def contacts(request):
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
     return render(request, 'mainApp/contact.html', locals())
 
